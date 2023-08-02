ed663c4c3593e24dfd2e8223e732707fffb133ad
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-08-02 18:47:39 +0200

    banner

--- src/core/components/header/header.component.ts
+++ src/core/components/header/header.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  public text: string = 'Texto de ejemplo que se mueve de derecha a izquierda';
+  public scrollSpeed: number = 1; // Velocidad de desplazamiento (ajusta según tus preferencias)
+
+  constructor() { }
+
+  ngOnInit() {
+    this.startScrolling();
+  }
+
+  startScrolling() {
+    const container = document.getElementById('textBannerContainer');
+    const banner = document.getElementById('textBanner');
+
+    if (container && banner) {
+      let position = container.offsetWidth;
+
+      const moveBanner = () => {
+        position--;
+        if (position < -banner.offsetWidth) {
+          position = container.offsetWidth;
+        }
+        banner.style.left = position + 'px';
+        requestAnimationFrame(moveBanner);
+      };
+
+      moveBanner();
+    }
+  }
 
 }
