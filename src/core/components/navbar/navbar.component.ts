5d462fd3fa178db26de521d057a03fce0774de91
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-12-03 10:45:53 +0100

    avatar

--- src/core/components/navbar/navbar.component.ts
+++ src/core/components/navbar/navbar.component.ts
@@ -6,6 +6,7 @@ import { UsersService } from 'src/core/services/users/users.service';
 import { logoService } from 'src/core/services/logo/logo.service';
 import { FormControl, FormGroup } from '@angular/forms';
 
+
 // interface City {
 //   name: string;
 //   code: string;
@@ -18,26 +19,14 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class NavbarComponent implements OnInit {
   logoUrl: string = '';
-  // logos: string[] = [
-  //   //  'assets/images/logo.png',
-  //   'assets/images/logo1.png',
-  //   'assets/images/logo2.png',
-  //   'assets/images/logo4.png',
-  //   'assets/images/logo6.png',
-  //   'assets/images/logo7.png',
-  //   'assets/images/logo8.png',
-  //   //  'assets/images/logo5.jpeg',
-  //   //  'assets/images/logo6.jpeg',
-  //   //  'assets/images/logo7.jpeg',
-  //   //  'assets/images/logo8.jpeg',
-  //   //  'assets/images/logo9.jpeg',
-  // ];
   formGroup: FormGroup | undefined;
 
   carts: number = 0;
   activeUser: any;
   activeUserName: any;
   totalUnidades: number = 0;
+
+  palabrasAvatar='';
 
   constructor(
     private router: Router,
@@ -63,33 +52,31 @@ export class NavbarComponent implements OnInit {
       console.log(this.activeUser);
       if (this.activeUser) {
         this.activeUserName = this.activeUser.data.user;
+        this.lettersAvatar(this.activeUserName)
         console.log(this.activeUserName);
       }
     });
-    //this.logoService.changeLogo();
-    // this.logoUrl = this.logoService.getLogoUrl();
-    // console.log(this.logoUrl); // Accede a la URL actual del logo
-   
-    // setInterval(() => this.logoService.changeLogo(), 5000);
     this.logoService.logoUrl$.subscribe(newLogoUrl => {
       this.logoUrl = newLogoUrl;
     });
-
-    // this.changeLogo();
-    // setInterval(() => this.changeLogo(), 5000);
-
-    // this.formGroup = new FormGroup({
-    //   selectedCity: new FormControl<City | null>(null),
-    // });
   }
   logout(): void {
     this.usersService.clearCurrentUser();
     this.router.navigate(['/']);
   }
 
-  // changeLogo() {
-  //   console.log('Entro');
-  //   const randomIndex = Math.floor(Math.random() * this.logos.length);
-  //   this.logoUrl = this.logos[randomIndex];
-  // }
+   lettersAvatar(cadena: string, cantidadLetras = 1) {
+    // Dividir la cadena en palabras
+    const palabras = cadena.split(' ');
+  
+    // Iterar sobre cada palabra y extraer las letras especificadas
+    const letrasExtraidas = palabras.map((palabra) => {
+      return palabra.slice(0, cantidadLetras);
+    });
+  
+    // Unir las letras extraídas en una nueva cadena
+    const resultado = letrasExtraidas.join('');
+  
+    this.palabrasAvatar= resultado;
+  }
 }
