fc0daaf89f63da67c26b9416b1281f2a3ca4e16c
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2024-01-02 20:05:09 +0100

    boton scroll

---
+++ src/pages/products/card-play/card-play.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardPlayComponent } from './card-play.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardPlayComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CardPlayModule { }
